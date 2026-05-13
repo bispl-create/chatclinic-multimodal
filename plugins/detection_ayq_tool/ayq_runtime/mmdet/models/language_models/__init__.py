69289e068fe7ded49b9965cf128acf036e06d05e
Hyunmin Hwang <hwangkei0212@gmail.com>
2026-05-13 21:37:42 +0900

    Revert "Add AYQ Detection Plugin"

--- plugins/detection_ayq_tool/ayq_runtime/mmdet/models/language_models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .bert import BertModel
-from .on_the_fly_text_encoder import OnTheFlyTextEncoder
-
-__all__ = ['BertModel','OnTheFlyTextEncoder']
